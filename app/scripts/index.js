125f778586b88ccb41fc2823bf1f9268cac9300f
git-directory-deploy <geraldemory@Geralds-MacBook-Pro.local>
2017-09-06 14:50:11 -0400

    added a now playing

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,53 +1,53 @@
 let $ = require('jquery');
 let handlebars = require('handlebars');
 let inputValue = document.getElementById("searchButton");
-  //console.log(musicUrl);
-  inputValue.addEventListener("click", function(){
-    clearSearchResults();
-    doSearch();
-  });
+//console.log(musicUrl);
+inputValue.addEventListener("click", function() {
+  clearSearchResults();
+  doSearch();
+});
 
 
-  function clearSearchResults(){
-    $('.results').html('');
-  }
+function clearSearchResults() {
+  $('.results').html('');
+}
 
 function doSearch() {
 
   let musicValue = document.getElementById('searching').value;
   let musicUrl = 'https://itunes.apple.com/search';
-  fetch(musicUrl + '?term=' + musicValue +'&country=US&media=music&limit=15').then(function(res){
-      return res.json();
+  fetch(musicUrl + '?term=' + musicValue + '&country=US&media=music&limit=15').then(function(res) {
+    return res.json();
   }).then(showSearchResults);
 
 }
 
 
-function showSearchResults(ajaxResults){
-    let musicResults = ajaxResults.results;
-    console.log(ajaxResults);
-    displayMusic(musicResults);
+function showSearchResults(ajaxResults) {
+  let musicResults = ajaxResults.results;
+  console.log(ajaxResults);
+  displayMusic(musicResults);
 }
 
 function displayMusic(musicResults) {
   let source = $('#music-template').html();
   let template = handlebars.compile(source);
 
-  musicResults.forEach(function(music){
+  musicResults.forEach(function(music) {
     let searchResultHtml = $(template(music));
 
 
-   //console.log(music.previewUrl);
-   searchResultHtml.find('.image').data('music', music)
-  //  console.log($(searchResultHtml).find('.image'));
-   searchResultHtml.find('.image').click(playMusic)
+    //console.log(music.previewUrl);
+    searchResultHtml.find('.image').data('music', music)
+    //  console.log($(searchResultHtml).find('.image'));
+    searchResultHtml.find('.image').click(playMusic)
 
-   $('.results').append(searchResultHtml);
+    $('.results').append(searchResultHtml);
 
-  //  let imageSource = document.querySelectorAll('.image')
-  //  imageSource.forEach(function(item, counter){
-  //    imageSource[counter].addEventListener('click', playMusic);
-  //  });
+    //  let imageSource = document.querySelectorAll('.image')
+    //  imageSource.forEach(function(item, counter){
+    //    imageSource[counter].addEventListener('click', playMusic);
+    //  });
 
   });
 }
